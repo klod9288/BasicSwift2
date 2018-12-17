b19e444edc787cf26c5b4d51dbd1c285afb820a5
srongklod <klod9288@gmail.com>
2018-12-17 13:15:25 +0700

    If else

---
+++ BasicSwift.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+//Implicit Variable and constant
+var nameString = "Master Klod"
+var number1:Int = 123
+
+nameString = "Dolemon"
+let name2Strong = "Nopita"
+//name2Strong = "TestNopita" ไม่สามารถ compile ได้ เพราะ let ไม่สามารถแก้ไขได้แบบ var
+
+//Explicit
+
+
+//show console
+
+
+//if else Statement
+var status1:Bool = true
+
+if status1 {
+    
+    print("status True")
+    
+}else{
+    
+    print("status False")
+    
+}
+
+//Convert Data Type
+var number4:Int = 200
+var number5:Double=25.00
+var number6:String="50"
+
+
+var anawer1: Int = number4 + 100 - Int(number5)+Int(number6)!
+
+//About String
+var s1:String = "Dog"
+var s2:String = " "
+var s3:String = "Cat"
+
+var result1:String = s1+s2+s3 //Java Type
+
+
