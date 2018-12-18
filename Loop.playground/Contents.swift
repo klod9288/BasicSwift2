a27d89b4ee9ff8629c874802f22b2db73605f517
srongklod <klod9288@gmail.com>
2018-12-18 11:15:39 +0700

    for and while loop finish

---
+++ Loop.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+//For Loop Statment
+var friendStrings = ["Hero","Play","Set 100"]
+for itemFriend in friendStrings {
+    print("I Love \(itemFriend)")
+}
+
+//While Loop Statement
+var count: Int = 1
+
+while count <= 10 {
+    print("Value Count ==>\(count)")
+    count += 1
+    
+    
+}
+
+
